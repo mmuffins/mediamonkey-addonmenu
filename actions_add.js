c04f97d5af14453160a94b6e6a4ec2e7dea384c2
mmuffins <mail@tiv73.com>
2018-03-01 17:59:51 +0100

    rewrote addon

--- actions_add.js
+++ actions_add.js
@@ -1,65 +1,130 @@
 
 // Add global section to register addon actions if it doesn't exist yet
-if(typeof addons == "undefined"){
-	var addons = {}
-}
-
+if (typeof addons == "undefined") var addons = {}
 
 window.actionCategories.addons = function(){
 	return _('AddonsMenu');
 }
 
-let addonsMenu = {
-		action: {
-				title: function () {
-						return _('&Addons');
+addons.addonsMenuQueue = []
+
+addons.addonsMenu = {
+	menuOrder: 55,
+	menuGrouporder: 10,
+	menu: [],
+
+
+	  registerCommands: function (actions) {
+		// Adds the provided actions to the Addons Menu
+		let _this = this
+
+		return new Promise((resolve, reject) => {
+			if(!actions instanceof Array) return resolve(false);
+
+			actions.forEach(el => {
+				if(!el.hasOwnProperty('action') ||  el.action.title() == '' 
+				|| !el.hasOwnProperty('order') || !el.hasOwnProperty('category')){
+					return
+				}
+	
+				if (typeof _this.menu.find(action => action.title ==  title && action.category == category) != "undefined") {
+					// action already exists
+					return
+				} 
+	
+				let newItem = {action: el.action, order: el.order, category: el.category, title:el.action.title(), grouporder: 100}
+	
+				// console.log('loading action => ' + newItem)
+				_this.menu.push(newItem)
+	
+			})
+	
+			return resolve(true)
+		}).then(() => _this.pushToUi())
+	  },
+
+	pushToUi: function(){
+		// pushes the internal menu to the ui
+
+		let _this = this
+
+		return new Promise(function (resolve, reject) {
+			_this.sortMenu();
+			let menuItems = []
+	
+			_this.menu.forEach(el => {
+				if(!el.hasOwnProperty('action') || !el.hasOwnProperty('order') 
+					|| !el.hasOwnProperty('grouporder') || !el.hasOwnProperty('category')){
+					return
+				}
+				menuItems.push({action: el.action, order: el.order, grouporder : el.grouporder, category: el.category})
+			})
+	
+			let newMenu = {
+				action: {
+					title: function () {
+							return _('&Addons');
+					},
+					visible: !webApp,
+					submenu: menuItems
 				},
-				visible: !webApp,
-				submenu: [
-						{
-								// action: actions.fixArtistDelimiter,
-								order: 10,
-								grouporder: 10
-						},
-						// {
-						// 		action: 'mp playlist checker',
-						// 		order: 20,
-						// 		grouporder: 10
-						// },
-						{
-								// action: actions.musicLifecycle.new,
-								order: 10,
-								grouporder: 100
-						},
-						{
-								// action: actions.musicLifecycle.newMusic,
-								order: 20,
-								grouporder: 100
-						},
-						{
-								// action: actions.musicLifecycle.pending,
-								order: 30,
-								grouporder: 100
-						},
-						{
-								// action: actions.musicLifecycle.transit,
-								order: 40,
-								grouporder: 100
-						},
-						{
-								// action: actions.musicLifecycle.looped,
-								order: 50,
-								grouporder: 100
-						},
-						{
-								// action: actions.musicLifecycle.active,
-								order: 60,
-								grouporder: 100
-						}
-				]
-		},
-		order: 55,
-		grouporder: 10,
+				order: _this.menuOrder,
+				grouporder: _this.menuGrouporder,
+			}
+	
+			for (let i = 0; i < window.mainMenuItems.length; i++) {
+				const el = window.mainMenuItems[i].action;
+				if(el.title() == '&Addons'){
+					window.mainMenuItems[i] = newMenu
+					return
+				}
+			}			
+			return resolve(newMenu)
+		})
+		.then(newMenu => window.mainMenuItems.push(newMenu))
+	},
+
+	getAction: function(title, category){
+		// returns a reference to the action with the provided title and category
+		return this.menu.find(action => action.title ===  title && action.category === category)		 
+	},
+
+	getCategories: function(){
+		// returns an array with all categories in the menu
+		return [...new Set(this.menu.map(x => x.category))]
+	},
+
+	sortMenu: function(){
+		// sorts the menu by category
+
+		// get list of all categories and add a sort value
+		let cat = this.getCategories().sort()
+		let catOrder = {}
+		for (let index = 0; index < cat.length; index++) {
+			catOrder[cat[index]] = (index + 1) * 100
+		}
+
+		// update each menu item with the sort index
+		this.menu.forEach(el => {
+			el.grouporder = catOrder[el.category]
+		});
+	},
+
+	importMenuQueue: function(){
+		// checks addons.addonsMenuQueue for new entries and adds them to the menu
+
+		if (!addons.hasOwnProperty('addonsMenuQueue')) {
+			addons.addonsMenuQueue = []
+			return
+		}
+
+		this.registerCommands(addons.addonsMenuQueue)
+		.then(addons.addonsMenuQueue = [])
+		
+		
+	}
 }
 
-window.mainMenuItems.push(addonsMenu)
+// addons.addonsMenu.importMenuQueue()
+
+
