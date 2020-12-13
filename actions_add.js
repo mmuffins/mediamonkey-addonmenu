b1f62a89ec43a2f7dda0e5de5f9f656646cd4387
mmuffins <mail@tiv73.com>
2020-12-13 22:08:02 +0100

    Fix exception at first start

--- actions_add.js
+++ actions_add.js
@@ -1,1 +1,5 @@
+// Add global section to register extension actions if it doesn't exist yet
+if(typeof extensions == "undefined" )
+	extensions = {};
+
 actionCategories.extensions = () => _('Extensions');
